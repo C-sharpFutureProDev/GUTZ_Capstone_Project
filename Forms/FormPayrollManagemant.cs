53d4ecbe85ccbe2d665f847a371588b23626213e
C-sharpFutureProDev <justit1694@gmail.com>
2024-07-21 21:07:54 +0800

    Added necessary forms.

--- Forms/FormPayrollManagemant.cs
+++ Forms/FormPayrollManagemant.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GUTZ_Capstone_Project
+namespace GUTZ_Capstone_Project.Forms
 {
-    public partial class Form1 : Form
+    public partial class FormPayrollManagemant : Form
     {
-        public Form1()
+        public FormPayrollManagemant()
         {
             InitializeComponent();
         }
